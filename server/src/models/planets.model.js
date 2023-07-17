a2bbb889c1489298ba2927a927e0cbdd44d72202
Dave Lin <dave.lin21@gmail.com>
2023-07-17 14:08:29 -0500

    set up npm automations

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,46 @@
-const planets = [];
+const { parse } = require('csv-parse');
+const path = require('path');
+const fs = require('fs');
 
-module.exports = planets;
+const results = [];
+
+const isHabitablePlanet = (planet) => {
+  return (
+    planet['koi_disposition'] === 'CONFIRMED' &&
+    planet['koi_insol'] > 0.36 &&
+    planet['koi_insol'] < 1.11 &&
+    planet['koi_prad'] < 1.6
+  );
+};
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, '..', '..', 'data', 'kepler_data.csv')
+    )
+      .pipe(
+        parse({
+          comment: '#',
+          columns: true,
+        })
+      )
+      .on('data', (data) => {
+        if (isHabitablePlanet(data)) {
+          results.push(data);
+        }
+      })
+      .on('error', (err) => {
+        console.log(err);
+        reject(err);
+      })
+      .on('end', () => {
+        console.log(`habitable planets: ${results.length}`);
+        resolve();
+      });
+  });
+}
+
+module.exports = {
+  loadPlanetsData,
+  planets: results,
+};
