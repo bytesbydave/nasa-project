37fa9b376aa266dde706ad48b3575472ada74f25
Dave Lin <dave.lin21@gmail.com>
2023-07-21 00:08:10 -0500

    exit launch functionality to mongoose

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,7 +1,7 @@
 const launchesDatabase = require('./launches.mongo');
-// const launches = new Map();
+const planets = require('./planets.mongo');
 
-let latestFlightNumber = 100;
+const DEFAULT_FLIGHT_NUMBER = 100;
 
 const launch = {
   flightNumber: 100,
@@ -20,8 +20,23 @@ async function getAllLaunches() {
   return await launchesDatabase.find({}, { __: 0, __v: 0 });
 }
 
+async function getLatestFlightNumber() {
+  const latestLaunch = await launchesDatabase.findOne().sort('-flightNumber');
+
+  if (!latestLaunch) {
+    return DEFAULT_FLIGHT_NUMBER;
+  }
+  return latestLaunch.flightNumber;
+}
+
 async function saveLaunch(launch) {
-  await launchesDatabase.updateOne(
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+  if (!planet) {
+    throw new Error('No matching planet found');
+  }
+  await launchesDatabase.findOneAndUpdate(
     {
       flightNumber: launch.flightNumber,
     },
@@ -30,33 +45,44 @@ async function saveLaunch(launch) {
   );
 }
 
-function addNewLaunch(launch) {
-  latestFlightNumber++;
-  launches.set(
-    launch.flightNumber,
-    Object.assign(launch, {
-      customers: ['NASA', 'BASA'],
-      upcoming: true,
-      success: true,
-      flightNumber: latestFlightNumber,
-    })
+async function scheduleNewLaunch(launch) {
+  const newFlightNumber = (await getLatestFlightNumber()) + 1;
+
+  const newLaunch = Object.assign(launch, {
+    success: true,
+    upcoming: true,
+    customers: ['Nasa', 'BASA'],
+    flightNumber: newFlightNumber,
+  });
+  await saveLaunch(newLaunch);
+}
+
+async function existsLaunchWithId(launchId) {
+  return await launchesDatabase.findOne({
+    flightNumber: launchId,
+  });
+}
+
+async function abortLaunchById(launchId) {
+  const aborted =  await launchesDatabase.updateOne(
+    {
+      flightNumber: launchId,
+    },
+    {
+      upcoming: false,
+      success: false,
+    }
   );
-}
-
-function existsLaunchWithId(launchId) {
-  return launches.has(launchId);
-}
-
-function abortLaunchById(launchId) {
-  const aborted = launches.get(launchId);
-  aborted.upcoming = false;
-  aborted.success = false;
-  return aborted;
+  return aborted.modifiedCount === 1
+  // const aborted = launches.get(launchId);
+  // aborted.upcoming = false;
+  // aborted.success = false;
+  // return aborted;
 }
 
 module.exports = {
   getAllLaunches,
-  addNewLaunch,
   existsLaunchWithId,
+  scheduleNewLaunch,
   abortLaunchById,
 };
