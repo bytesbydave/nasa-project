0b7c061c0a456735d222620bce80df232b08d850
Dave Lin <dave.lin21@gmail.com>
2023-07-26 11:55:14 -0500

    add google auth

--- server/src/app.js
+++ server/src/app.js
@@ -3,18 +3,35 @@ const path = require('path');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
+
 require('dotenv').config();
 
 const api = require('./routes/api');
-
-const app = express();
-
-app.use(helmet());
 
 const config = {
   CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
 };
+
+const AUTH_OPTIONS = {
+  callbackURL: '/auth/google/callback',
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log('Google profile', profile);
+  done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
+const app = express();
+
+app.use(helmet());
+app.use(passport.initialize());
 
 function checkLoggedIn(req, res, next) {
   const isLoggedIn = true; // TODO: check if isLoggedIn, res status 401
@@ -36,14 +53,33 @@ app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.get('/auth/google', (req, res) => {});
-app.get('/auth/google/callback', (req, res) => {});
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['email', 'profile'],
+  })
+);
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/failure',
+    successRedirect: '/',
+    session: false,
+  }),
+  (req, res) => {
+    console.log('Google Called us back');
+  }
+);
 app.get('/auth/loggout', (req, res) => {});
 
 app.use('/v1', api);
 
 app.get('/secret', checkLoggedIn, (req, res) => {
   return res.send('Your personal secrett value is 42');
+});
+
+app.get('/failure', (req, res) => {
+  return res.send('Failed to log in');
 });
 
 app.get('/*', (req, res) => {
