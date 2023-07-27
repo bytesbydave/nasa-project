c135cdaf7d4217e9b05a356c601cfb987324c70d
Dave Lin <dave.lin21@gmail.com>
2023-07-26 19:01:41 -0500

    pass client tests and adjust cicd db

--- server/src/routes/launches/launches.spec.js
+++ server/src/routes/launches/launches.spec.js
@@ -1,11 +1,17 @@
 const request = require('supertest');
 const app = require('../../app');
 const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
+const { loadPlanetsData } = require('../../models/planets.model');
 
 describe('Launches API', () => {
   beforeAll(async () => {
     await mongoConnect();
+    await loadPlanetsData();
   });
+
+  // afterAll(async () => {
+  //   await mongoDisconnect();
+  // });
 
   describe('GET /launches', () => {
     it('should respond with 200 success', async () => {
